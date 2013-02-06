4a3fd244c127acf87673c863f45066630efae5df
John Garbutt <john.garbutt@citrix.com>
2013-02-06 15:31:25 +0000

    extract xenapi plugins
    Change-Id: Ib85b04b02eb7bdd776fdb747804842311ca4725d

--- tools/XenAPI/create_domU_VM_and_run_devstack.sh
+++ tools/XenAPI/create_domU_VM_and_run_devstack.sh
@@ -10,40 +10,8 @@ THIS_DIR=$(cd $(dirname "$0") && pwd)
 . $THIS_DIR/scripts/on_exit.sh
 cd $THIS_DIR
 
-generate_zipball_url()
-{
-    REPO=$1
-    BRANCH=$2
-    echo $REPO | sed "s:\.git$::;s:$:/zipball/$BRANCH:g"
-}
-
-NOVA_ZIPBALL_URL=${NOVA_ZIPBALL_URL:-$(generate_zipball_url($NOVA_REPO $NOVA_BRANCH))}
-install_xenapi_plugins $NOVA_ZIPBALL_URL "plugins/xenserver/xenapi/etc/xapi.d/plugins/*"
-
-if [[ "$ENABLED_SERVICES" =~ "q-agt" && "$Q_PLUGIN" = "openvswitch" ]]; then
-    QUANTUM_ZIPBALL_URL=${QUANTUM_ZIPBALL_URL:-$(echo $QUANTUM_REPO | sed "s:\.git$::;s:$:/zipball/$QUANTUM_BRANCH:g")}
-    install_xenapi_plugins $QUANTUM_ZIPBALL_URL "quantum/plugins/openvswitch/agent/xenapi/etc/xapi.d/plugins/*"
-fi
-
-
-ZIPBALL_URL=$1
-PLUGIN_LOCATION=$2
-
-wget $ZIPBALL_URL -O zipball --no-check-certificate
-
-mkdir tmp
-unzip -o zipball -d ./tmp
-
-XAPI_PLUGIN_DIR=/etc/xapi.d/plugins/
-if [ ! -d $XAPI_PLUGIN_DIR ]; then
-    XAPI_PLUGIN_DIR=/usr/lib/xcp/plugins/
-fi
-
-cp -pr "./tmp/*/$PLUGIN_LOCATION"  $XAPI_PLUGIN_DIR
-rm -rf ./tmp
-
-chmod a+x ${XAPI_PLUGIN_DIR}*
-mkdir -p /boot/guest
+source lib/xenapi_plugins.sh
+install_xenapi_plugins
 
 #
 # Configure Networking
